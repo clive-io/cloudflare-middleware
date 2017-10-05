6e26a6dcc46f425da2221199181f0fdbbfdae5ca
Clive Chan <cc@clive.io>
2017-10-05 12:15:00 -0400

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ranges = require("./ranges");
 module.exports = function(options){
   return function(req, res, next) {
     if(req.ip.startsWith("::ffff:"))
-      req.ip = req.ip.slice(7);
+      req.ip = req.ip.substr(7);
     if(rangeCheck.inRange(req.ip, ranges)){
       req.ip = req.headers['cf-connecting-ip'];
       next();
