fed2353f2ee10e9627c8923863fef76ed2468dcd
Clive Chan <cc@clive.io>
2017-10-05 12:06:04 -0400

    rip ipv6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const ranges = require("./ranges");
 
 module.exports = function(options){
   return function(req, res, next) {
+    if(req.ip.startsWith("::ffff:"))
+      req.ip = req.ip.slice(7);
     if(rangeCheck.inRange(req.ip, ranges)){
       req.ip = req.headers['cf-connecting-ip'];
       next();
