7e46ca0667e046369fc805a0736ddd21380b07b8
Clive Chan <cc@clive.io>
2017-10-14 02:05:01 -0400

    Use origin_ip properly

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const ranges = require("./ranges");
 
 module.exports = function(options){
   return function(req, res, next) {
-    if(req.ip.startsWith("::ffff:"))
-      req.ip = req.ip.substr(7);
-    if(rangeCheck.inRange(req.ip, ranges)){
-      req.ip = req.headers['cf-connecting-ip'];
+    ip = req.ip;
+    if(ip.startsWith("::ffff:"))
+      ip = ip.substr(7);
+    if(rangeCheck.inRange(ip, ranges)){
+      req.origin_ip = req.headers['cf-connecting-ip'];
       next();
     }else{
       res.end();
