a5ea996c2a27ac88e3a2dcc8e5bf4457b67cb27a
Paul Melnikow <email@paulmelnikow.com>
2020-10-15 17:32:35 -0400

    Support trust proxy and other frameworks

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const rangeCheck = require("range_check");
-const ranges = require("./ranges");
+const rangeCheck = require('range_check')
+const ranges = require('./ranges')
 
-module.exports = function(options){
-  return function(req, res, next) {
-    ip = req.ip;
-    if(ip.startsWith("::ffff:"))
-      ip = ip.substr(7);
-    if(rangeCheck.inRange(ip, ranges)){
-      req.origin_ip = req.headers['cf-connecting-ip'];
-      next();
-    }else{
-      res.end();
+function trimIp(ip) {
+  return ip.startsWith('::ffff:') ? ip.substr(7) : ip
+}
+
+function isCloudflare(ip) {
+  return rangeCheck.inRange(ip, ranges)
+}
+
+function trustProxy(ip) {
+  const trimmed = trimIp(ip)
+  return trimmed == '127.0.0.1' || isCloudflare(trimmed)
+}
+
+function middleware(options) {
+  return function (req, res, next) {
+    if (isCloudflare(trimIp(req.ip))) {
+      req.origin_ip = req.headers['cf-connecting-ip']
+      next()
+    } else {
+      res.end()
     }
-  };
-};
+  }
+}
+
+module.exports = {
+  isCloudflare,
+  middleware,
+  trustProxy,
+}
